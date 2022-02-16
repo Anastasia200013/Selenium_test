06702db66b6bd9914dfa26a42358f37306fefe9b
a.petrova <getupnastya2000@gmail.com>
2022-02-16 16:41:52 +0300

    test add

--- test_selen.py
+++ test_selen.py
@@ -13,32 +13,26 @@ def test_passing():
     search_home = driver.find_element(By.CSS_SELECTOR, "li.ng-binding.ng-scope a").click()
     time.sleep(5)
 
-    login_1 = driver.find_element(By.CSS_SELECTOR, 'div.modal-login div.modal-content div.form-group '
-                                                   '#input_login_01').send_keys('Sysdba')
+    login_1 = driver.find_element(By.CSS_SELECTOR, '#input_login_01').send_keys('Sysdba')
 
-    parol_1 = driver.find_element(By.CSS_SELECTOR, 'div.modal-login div.modal-content div.form-group '
-                                                   '#input_password_01').send_keys('305')
+    parol_1 = driver.find_element(By.CSS_SELECTOR, '#input_password_01').send_keys('305')
 
     time.sleep(3)
     button_enter = driver.find_element(By.CSS_SELECTOR, '#btn_login_accept_01.btn.btn-primary.btn-modal').click()
 
     time.sleep(5)
-    search_directory = driver.find_element(By.CSS_SELECTOR, 'div.collapse.navbar-collapse ul.nav.navbar-nav '
-                                                            'li.ng-scope:nth-child(7) a').click()
+    search_directory = driver.find_element(By.CSS_SELECTOR, 'ul.nav.navbar-nav li.ng-scope:nth-child(7) a').click()
 
     time.sleep(3)
-    search_col_record = driver.find_element(By.CSS_SELECTOR, 'div.container.ng-scope div.ng-isolate-scope '
-                                                             'div.div-scroll-x.col-sm-10 div#pg-balances-top '
-                                                             'div.btn.btn-default.pull-right.disabled  b.ng-binding')
+    search_col_record = driver.find_element(By.CSS_SELECTOR, 'div#pg-balances-top div.btn.btn-default.'
+                                                             'pull-right.disabled  b.ng-binding')
 
     assert search_col_record.text == '2982'
 
     def test_check_results_count(driver):
         driver.execute_script("window.scrollBy(0, 350);")
         time.sleep(3)
-        search_list = driver.find_elements(By.CSS_SELECTOR, 'div.container.ng-scope div.ng-isolate-scope '
-                                                            'div.div-scroll-x.col-sm-10 div.ng-isolate-scope '
-                                                            'table.table.table-bordered.table-hover tbody tr')
+        search_list = driver.find_elements(By.CSS_SELECTOR, 'table.table.table-bordered.table-hover tbody tr')
         return len(search_list) <= 20
 
     test_check_results_count(driver)
@@ -70,15 +64,9 @@ def test_passing():
     dropdown_list = driver.find_element(By.CSS_SELECTOR, 'div.row div.col-sm-3.hidden-xs '
                                                          'div.panel-heading.pointer-cursor:nth-child(1)').click()
     time.sleep(3)
-    list_substr = driver.find_element(By.CSS_SELECTOR, 'div.container.ng-scope div.row div.col-sm-3 '
-                                                       'div.panel-body.collapse.in input')
+    list_substr = driver.find_element(By.CSS_SELECTOR, 'div.panel-body.collapse.in input')
     list_substr.send_keys('Содержание жилья')
     time.sleep(1)
-    button_ok = driver.find_element(By.CSS_SELECTOR, 'body > div > div.container.ng-scope > div > '
-                                                     'div:nth-child(2) > div:nth-child(2) > div > '
-                                                     'div.panel-body.collapse.in > form > div.form-group-buttons > '
-                                                     'button.btn.btn-primary').click()
+    button_ok = driver.find_element(By.CSS_SELECTOR, 'div.form-group-buttons button.btn.btn-primary').click()
     time.sleep(5)
-    search_price = driver.find_element(By.CSS_SELECTOR, 'div.page.ng-scope div.container.ng-scope '
-                                                        'div.col-sm-2.ng-isolate-scope ul.nav.nav-stacked.side-pills '
-                                                        'li.pointer-cursor.ng-scope:nth-child(2)').click()
+    search_price = driver.find_element(By.CSS_SELECTOR, 'li.pointer-cursor.ng-scope:nth-child(2)').click()
