fd15a06daaa89f22cf6b40529bebe6bfebdc712b
a.petrova <getupnastya2000@gmail.com>
2022-03-16 15:38:55 +0300

    test yaml project

--- MainTesting/Test_YAML.py
+++ MainTesting/Test_YAML.py
@@ -49,6 +49,7 @@ def auth_user(user_name, password, driver_init):
     button_enter.send_keys(Keys.RETURN)
     time.sleep(4)
 
+
 # переход на нужную старницу
 def perechod(driver_init):
     search_abonent = wait_of_element_located(selector='div#navbar_collapse_01 ul.nav.navbar-nav:nth-child(1) '
@@ -56,14 +57,15 @@ def perechod(driver_init):
     search_abonent.send_keys(Keys.RETURN)
     time.sleep(6)
 
+
 # Тест
 def test_add_jacket_to_the_shopcart(driver_init):
     # Аутентификация пользователя
     with open('D:\Fixtures\Fixture.yml', encoding='utf-8') as fd:
         # Load YAML data from the file
         read_options = dict(yaml.load(fd, Loader=SafeLoader))
-    login_user = str(read_options['TestUsers']['Login'])
-    password_user = str(read_options['TestUsers']['Password'])
+    login_user = str(read_options['TestUsers'][1]['Login'])
+    password_user = str(read_options['TestUsers'][1]['Password'])
 
     auth_user(login_user, password_user, driver_init=driver_init)
 
@@ -72,28 +74,27 @@ def test_add_jacket_to_the_shopcart(driver_init):
     with open('C:\\Users\\a.petrova\\PycharmProjects\\selenium-test\\TestsYML\\Test.yml', encoding='utf-8') as f:
         # Load YAML data from the file
         read_data_test = dict(yaml.load(f, Loader=SafeLoader))
-    #file_name = str(read_data_test['TestDescription']['FileSelectors'])
-    test_reg_ab = str(read_data_test['TestDescription']['TestAccount'])
-    test_reg_fio = str(read_data_test['TestDescription']['TestFIO'])
-    test_reg_address = str(read_data_test['TestDescription']['TestAddress'])
-    test_reg_ow = str(read_data_test['TestDescription']['TestInfoOwner'])
-    print(test_reg_ab, ';', test_reg_fio, ';', test_reg_address, ';', test_reg_ow)
+    # file_name = str(read_data_test['TestDescription']['FileSelectors'])
+    test_reg_ab = str(read_data_test['TestDescription']['Tests'][0]['Exp'])
+    test_reg_fio = str(read_data_test['TestDescription']['Tests'][1]['Exp'])
+    test_reg_address = str(read_data_test['TestDescription']['Tests'][2]['Exp'])
+    # test_reg_ow = str(read_data_test['TestDescription']['TestInfoOwner'])
+    print(test_reg_ab, ';', test_reg_fio, ';', test_reg_address)
 
-    with open('C:\\Users\\a.petrova\\PycharmProjects\\selenium-test\\SearchSlect\\'
-              'SearchSelectors.yml', encoding='utf-8') as h:
+    with open('C:\\Users\\a.petrova\\PycharmProjects\\selenium-test\\SearchSlect\\SearchSelectors.yml', encoding='utf-8') as h:
         # Load YAML data from the file
         read_data_slcrt = dict(yaml.load(h, Loader=SafeLoader))
 
-    selector_ab = str(read_data_slcrt['Selectors']['StrAccountcd'])
-    selector_fio = str(read_data_slcrt['Selectors']['StrFIO'])
-    selector_address = str(read_data_slcrt['Selectors']['StrAddressString'])
-    selector_ow = str(read_data_slcrt['Selectors']['StrOwnerid'])
-    print(selector_ab, ';', selector_fio, ';', selector_address, ';', selector_ow)
+    selector_ab = str(read_data_slcrt['Selectors']['Locations'][0]['Location'])
+    selector_fio = str(read_data_slcrt['Selectors']['Locations'][1]['Location'])
+    selector_address = str(read_data_slcrt['Selectors']['Locations'][2]['Location'])
+    # selector_ow = str(read_data_slcrt['Selectors']['StrOwnerid'])
+    print(selector_ab, ';', selector_fio, ';', selector_address)
 
     regex(selector_ab, test_reg_ab, driver_init=driver_init)
     regex(selector_fio, test_reg_fio, driver_init=driver_init)
     regex(selector_address, test_reg_address, driver_init=driver_init)
-    regex(selector_ow, test_reg_ow, driver_init=driver_init)
+    # regex(selector_ow, test_reg_ow, driver_init=driver_init)
 
 
 if __name__ == '__main__':
