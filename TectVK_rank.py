ca3a5c1b6cb5416371fdd9f2e795392d1d0117bf
Anastasia200013 <57151100+Anastasia200013@users.noreply.github.com>
2022-09-13 17:11:21 +0300

    Create TectVK_rank.py

---
+++ TectVK_rank.py
@@ -0,0 +1,12 @@
+from tasks import Task
+def test_dict():
+        task = Task('do something', 'okken', True, 21)
+        dict = task._asdict()
+        expected = {'summary': 'do something',
+                    'owner': 'okken',
+                    'done': True,
+                    'id': 21}
+        assert dict == expected
+
+
+
